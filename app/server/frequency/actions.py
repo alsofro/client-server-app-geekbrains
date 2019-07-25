46660bb9b64f903b10e16978759052b774d27154
asofronov <asofronov@rbc.ru>
2019-07-25 11:22:54 +0300

    add frequency module

---
+++ app/server/frequency/actions.py
@@ -0,0 +1,5 @@
+from .controllers import frequency_controller
+
+actionnames = [
+    {'action': 'frequency', 'controller': frequency_controller}
+]
---
+++ app/server/frequency/controllers.py
@@ -0,0 +1,13 @@
+from protocol import make_response
+import json
+
+def frequency_controller(request):
+    data = request.get('data')
+    frequency = {}
+    for letter in data:
+        if letter in frequency:
+            frequency[letter] += 1
+        else:
+            frequency[letter] = 1
+    frequency = json.dumps(frequency, ensure_ascii=False, indent=4)
+    return make_response(request, 200, frequency)
